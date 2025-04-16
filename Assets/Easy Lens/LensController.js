71047d11e99ee072acf1ea217b0e37553393bf73
Ulises Alvarenga <ulises.j.alvarenga@gmail.com>
2025-04-16 16:09:16 -0300

    First commit

---
+++ Assets/Easy Lens/LensController.js
@@ -0,0 +1,14 @@
+// Main Controller
+//
+// Made with Easy Lens
+
+
+
+try {
+
+// No code can be provided as the feature is not supported by the current blocks.
+
+} catch(e) {
+  print("error in controller");
+  print(e);
+}
